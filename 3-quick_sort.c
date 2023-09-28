c8fa149a05877d81623fde4de061e34aaccdbcbe
Zelalem <zzewdie@gmail.com>
2023-09-28 21:02:39 +0300

    sortalg_quick_v1

---
+++ 3-quick_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * partition - partitions an array using the Lomuto partition scheme
+ * @array: array to partition
+ * @low: starting index of partition
+ * @high: ending index of partition
+ * @size: size of the array
+ *
+ * Return: index of the pivot
+ */
+
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1, j, temp;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] > pivot)
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quicksort - sorts an array of integers in ascending order using the Quick
+ * sort algorithm
+ * @array: array to sort
+ * @low: starting index of partition
+ * @high: ending index of partition
+ * @size: size of the array
+ */
+
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = partition(array, low, high, size);
+		quicksort(array, low, pivot - 1, size);
+		quicksort(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - sorts an array of integers in ascending order using the Quick
+ * sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quicksort(array, 0, size - 1, size);
+}
