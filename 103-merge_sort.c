8e82472240af9a98b7b772863ef7657e80e4225c
Zelalem <zzewdie@gmail.com>
2023-10-02 21:51:28 +0300

    sortalg_merge_v2

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -3,85 +3,75 @@
 #include <stdio.h>
 
 /**
- * merge - merges two subarrays of an array
- * @array: array to sort
- * @left: left index of the subarray
- * @mid: middle index of the subarray
- * @right: right index of the subarray
- * @tmp: temporary array to store the sorted subarray
+ * merge - Merges two sub-arrays of an array
+ *
+ * @array: Array containing the sub-arrays to merge
+ * @size: Size of the array
+ * @left: Pointer to the left sub-array
+ * @left_size: Size of the left sub-array
+ * @right: Pointer to the right sub-array
+ * @right_size: Size of the right sub-array
  */
 
-void merge(int *array, int left, int mid, int right, int *tmp)
+void merge(int *array, size_t size, int *left, size_t left_size,
+		int *right, size_t right_size)
 {
-	int i = left, j = mid + 1, k = 0;
+	int i = 0, j = 0, k = 0;
+	int *output = malloc(sizeof(int) * size);
+
+	if (!output)
+		return;
 
 	printf("Merging...\n");
 	printf("[left]: ");
-	print_array(array + left, mid - left + 1);
+	print_array(left, left_size);
 	printf("[right]: ");
-	print_array(array + mid + 1, right - mid);
+	print_array(right, right_size);
 
-	while (i <= mid && j <= right)
+	while (i < (int)left_size && j < (int)right_size)
 	{
-		if (array[i] <= array[j])
-			tmp[k++] = array[i++];
+		if (left[i] <= right[j])
+			output[k++] = left[i++];
 		else
-			tmp[k++] = array[j++];
+			output[k++] = right[j++];
 	}
 
-	while (i <= mid)
-		tmp[k++] = array[i++];
-	while (j <= right)
-		tmp[k++] = array[j++];
+	while (i < (int)left_size)
+		output[k++] = left[i++];
 
-	for (i = left, k = 0; i <= right; i++, k++)
-		array[i] = tmp[k];
+	while (j < (int)right_size)
+		output[k++] = right[j++];
+
+	for (i = 0; i < (int)size; i++)
+		array[i] = output[i];
 
 	printf("[Done]: ");
-	print_array(array + left, right - left + 1);
+	print_array(output, size);
+
+	free(output);
 }
 
 /**
- * merge_sort_helper - sorts an array of integers in ascending order using the
- *                     Merge sort algorithm
- * @array: array to sort
- * @left: left index of the subarray
- * @right: right index of the subarray
- * @tmp: temporary array to store the sorted subarray
- */
-
-void merge_sort_helper(int *array, int left, int right, int *tmp)
-{
-	int mid;
-
-	if (left < right)
-	{
-		mid = (left + right) / 2;
-		merge_sort_helper(array, left, mid, tmp);
-		merge_sort_helper(array, mid + 1, right, tmp);
-		merge(array, left, mid, right, tmp);
-	}
-}
-
-/**
- * merge_sort - sorts an array of integers in ascending order using the
+ * merge_sort - Sorts an array of integers in ascending order using the
  *              Merge sort algorithm
- * @array: array to sort
- * @size: size of the array
+ *
+ * @array: Array to sort
+ * @size: Size of the array
  */
 
 void merge_sort(int *array, size_t size)
 {
-	int *tmp;
+	size_t mid = size / 2, i;
+	int *left, *right;
 
 	if (!array || size < 2)
 		return;
 
-	tmp = malloc(sizeof(int) * size);
-	if (!tmp)
-		return;
+	left = array;
+	right = array + mid;
 
-	merge_sort_helper(array, 0, size - 1, tmp);
+	merge_sort(left, mid);
+	merge_sort(right, size - mid);
 
-	free(tmp);
+	merge(array, size, left, mid, right, size - mid);
 }
